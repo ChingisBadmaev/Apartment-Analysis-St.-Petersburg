71e51ade4f5f073f584b1a0a02ca943c6a962ec6
ChingisBadmaev <chingis.badmaev2018@yandex.ru>
2020-09-25 20:53:15 +0300

    drop index

--- edit_csv_file.py
+++ edit_csv_file.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('house.csv', encoding='cp1251')
-
+DataF = pd.read_csv('UPDATE_HOUSE.csv', encoding='cp1251')
 # remove unnecessary
 df['Price'] = df['Price'].apply(lambda x: x[:-4])
 df['Metro distance'] = df['Metro distance'].apply(lambda x: str(x)[:-3])
@@ -11,5 +11,4 @@ df['Metro distance'] = df['Metro distance'].apply(lambda x: str(x)[:-3])
 Temp = df['Floor'].apply(lambda x: str.split(x, '/'))
 df['Floor'] = Temp.apply(lambda x: x[0])
 df['Max Floor'] = Temp.apply(lambda x: x[1])
-
-df.to_csv('UPDATE_HOUSE.csv')
+df.to_csv('UPDATE_HOUSE.csv', index=False)
