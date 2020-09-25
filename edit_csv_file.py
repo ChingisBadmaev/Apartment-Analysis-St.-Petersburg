b1bb941a74a02d76eeff271555685def570436e5
ChingisBadmaev <chingis.badmaev2018@yandex.ru>
2020-09-25 20:43:09 +0300

    edit the csv file

---
+++ edit_csv_file.py
@@ -0,0 +1,15 @@
+import csv
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('house.csv', encoding='cp1251')
+
+# remove unnecessary
+df['Price'] = df['Price'].apply(lambda x: x[:-4])
+df['Metro distance'] = df['Metro distance'].apply(lambda x: str(x)[:-3])
+
+Temp = df['Floor'].apply(lambda x: str.split(x, '/'))
+df['Floor'] = Temp.apply(lambda x: x[0])
+df['Max Floor'] = Temp.apply(lambda x: x[1])
+
+df.to_csv('UPDATE_HOUSE.csv')
